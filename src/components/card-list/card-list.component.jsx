3408fc2d06bfc54cbf04b49719075d5ce040fd5c
MattTIAN <jtia8705@icould.com>
2022-07-30 21:40:15 +1000

    rcc - Monsters Rolodex

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Card from "../card/card.component";
+import "./card-list.styles.css"
+export default class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          return (
+            <Card monster = {monster}/>
+          );
+        })}
+      </div>
+    );
+  }
+}
